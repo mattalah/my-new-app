0dfef0238454ee8f93e45af6d920b730bf21194e
saife <saifeddine.mattalah@craftfoundry.tech>
2024-01-11 10:58:04 +0100

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { routes } from "Config/routes";
+import { SignInPage } from "Pages/SignInPage";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 import './App.css'
+import { ErrorPage } from "Pages/ErrorPage";
+import { useMemo } from "react";
+import { ArticlePage, ArticleDetailPage } from "Pages/ArticlePage";
+import { SharedLayout } from "SharedLayout";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const routeList = useMemo(() => [{
+    path: routes.articles,
+    component: <ArticlePage />,
+    errorBoundary: true
+  }, {
+    path: `${routes.articleDetails}/:id`,
+    component: <ArticleDetailPage />,
+    errorBoundary: true
+  },
+  ], [])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={routes.signIn} element={<SignInPage />} />
+          <Route path="*"
+            element={
+              <ErrorPage
+                error={{
+                  statusCode: "404",
+                  title: "Page Not Found",
+                  message: "Sorry, we couldn’t find the page you’re looking for.",
+                }}
+              />
+            }
+          />
+          <Route element={<SharedLayout />}>
+            {routeList.map(route =>
+              <Route path={route.path}
+                key={route.path}
+                element={route.component}
+              />
+            )}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App
---
+++ src/Config/routes.ts
@@ -0,0 +1,9 @@
+export const routes = {
+  articles: "/articles",
+  articleAdd: "/articles/add",
+  articleDetails: "/articles/details",
+
+
+  signIn: "/sign-in",
+
+};
