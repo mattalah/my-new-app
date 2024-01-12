248a96c116ce464fadcf9d9ff8156ba98de25e95
saife <saifeddine.mattalah@craftfoundry.tech>
2024-01-12 06:17:45 +0100

    change register service

--- src/Service/AuthService.ts
+++ src/Service/AuthService.ts
@@ -1,14 +1,30 @@
 import Api from "./utils";
+import { toast } from "react-toastify";
 
-type MailSignIn = {
-    email: string;
-    password: string;
-}
 
-async function mailSignIn({ email, password }: MailSignIn) {
-    return await Api.post("oauth/sign-in", { email, password });
-}
+const register = async ({ email, password, name }: MailSignIn) => {
+    try {
+        const response = await Api.post("auth/register", { email, password, name });
+        toast.info("Successfully registered !", {
+            position: toast.POSITION.TOP_RIGHT
+        });
+        return response.data;
+    } catch (e: any) {
+        if (e.response.status == 422) {
+            toast.error("Already registered !", {
+                position: toast.POSITION.TOP_RIGHT
+            });
+            throw new Error("already_registered");
+
+        } else {
+            toast.error("Server Error !", {
+                position: toast.POSITION.TOP_RIGHT
+            });
+        }
+    }
+
+};
 
 export {
-    mailSignIn,
+    register,
 }
