d5c5352c060e78f08ec1141484da0dd0518ce6b6
saife <saifeddine.mattalah@craftfoundry.tech>
2024-01-12 12:44:33 +0100

    fix display of paginator

--- src/Components/PaginatorComponent/PaginatorComponent.tsx
+++ src/Components/PaginatorComponent/PaginatorComponent.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 
 interface PaginatorComponentProps {
     // statusCode: string;
@@ -5,16 +6,15 @@ interface PaginatorComponentProps {
     total: number;
     to: number;
     from: number;
-    links: {
-        link: string;
+    links?: {
+        link: () => string;
         number: number;
         isActive: boolean
     }[]
 }
 
-export const PaginatorComponent: React.FC<PaginatorComponentProps> = (data) => {
-    const { total, to, from, links } = data;
-    return <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+export const PaginatorComponent: React.FC<PaginatorComponentProps | undefined> = (data) => {
+    return (!data ? null : <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
         <div className="flex flex-1 justify-between sm:hidden">
             <a href="#" className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
             <a href="#" className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
@@ -23,11 +23,11 @@ export const PaginatorComponent: React.FC<PaginatorComponentProps> = (data) => {
             <div className="mr-4">
                 <p className="flex gap-1 text-sm text-gray-700 ">
                     <span>Showing</span>
-                    <span className="font-medium">{from}</span>
+                    <span className="font-medium">{data.from}</span>
                     to
-                    <span className="font-medium">{to}</span>
+                    <span className="font-medium">{data.to}</span>
                     of
-                    <span className="font-medium">{total}</span>
+                    <span className="font-medium">{data.total}</span>
                     <span>results</span>
                 </p>
             </div>
@@ -36,22 +36,23 @@ export const PaginatorComponent: React.FC<PaginatorComponentProps> = (data) => {
                     <a href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
                         <span className="sr-only">Previous</span>
                         <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
+                            <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
                         </svg>
                     </a>
-                    <a href="#" aria-current="page" className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">1</a>
-                    {links.map((link: any) =>
-                        <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">{link.number}</a>
+
+                    {data.links?.map((link: any) =>
+                        <NavLink key={link.number} to={link.link()} className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ring-1 ring-inset   focus:z-20  ${link.isActive ? 'z-10 bg-indigo-600 text-white focus-visible:outline-indigo-600 focus-visible:outline-offset-2 focus-visible:outline-2 focus-visible:outline' : ' ring-gray-300 text-gray-900 hover:bg-gray-50 focus:outline-offset-0'}`}>{link.number}</NavLink>
 
                     )}
                     <a href="#" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
                         <span className="sr-only">Next</span>
                         <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
+                            <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
                         </svg>
                     </a>
                 </nav>
             </div>
         </div>
     </div>
+    )
 }
